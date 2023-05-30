49ae21715d05b688c8acc7ae8a40f5bcd2448899
Tringa Baftiu <97124034+Triinga@users.noreply.github.com>
2023-05-30 02:28:11 +0200

    Update client.c

--- client.c
+++ client.c
@@ -47,3 +47,63 @@ int main(int argc, char const *argv[]) {
     	perror("[ERROR] CAN'T CONNECT TO THE HOST");
     	return -1;
 	}
+
+	// Create or get the message queue
+	int msgqid;
+	if ((msgqid = msgget(MSG_QUEUE_KEY, IPC_CREAT | 0666)) == -1) {
+    	perror("[ERROR] CAN'T CREATE/ACCESS MESSAGE QUEUE");
+    	return -1;
+	}
+
+	struct message msg;
+
+	while (1) {
+    	printf("[TCP]> ");
+    	fgets(input, BUFFER_SIZE, stdin);
+
+    	// Remove trailing newline character
+    	input[strcspn(input, "\n")] = '\0';
+
+    	if (strlen(input) == 0) {
+        	strcpy(input, "default message");
+    	} else if (strcmp(input, "exit") == 0) {
+        	printf("Client connection terminated\n");
+       	 
+        	// Send disconnect request to the message queue
+        	struct message disconnect_msg;
+        	disconnect_msg.mtype = 2; // Set message type for disconnect request
+        	strcpy(disconnect_msg.mtext, "disconnect"); // Set disconnect request message
+        	if (msgsnd(msgqid, &disconnect_msg, sizeof(disconnect_msg.mtext), 0) == -1) {
+            	perror("[ERROR] CAN'T SEND DISCONNECT REQUEST TO THE QUEUE");
+            	return -1;
+        	}
+       	 
+        	break;
+    	}
+
+    	// Send the request to the server
+    	send(sock, input, strlen(input), 0);
+    	//printf("[SEND] %s\n", input);
+
+    	// Send message to the message queue
+    	msg.mtype = 1; // Set message type
+    	strcpy(msg.mtext, input); // Copy input to message text
+    	if (msgsnd(msgqid, &msg, sizeof(msg.mtext), 0) == -1) {
+        	perror("[ERROR] CAN'T SEND MESSAGE TO THE QUEUE");
+        	return -1;
+    	}
+
+    	// Receive the response from the server
+    	valread = recv(sock, buffer, BUFFER_SIZE, 0);
+    	printf("[RECV] %s\n", buffer);
+
+    	// Clear the buffer
+    	memset(buffer, 0, BUFFER_SIZE);
+	}
+
+	close(sock);
+
+	return 0;
+}
+
+
